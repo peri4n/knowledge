4c0f6b483bd585ce137df639cc3f3f8cce639ea0
Fabian Bull <fabian@fbull.de>
2022-02-01 10:03:20 +0100

    Add Heap implementation in python

--- algds/ds/heap/max_heap.py
+++ algds/ds/heap/max_heap.py
@@ -1,15 +1,5 @@
-# Heap
+import unittest
 
-A *Heap* is a tree structure. There are many different kinds of Heaps each having different characteristics.
-
-## Max Heap
-
-A *Max Heap* is a Heap is a *binary tree* in which the element within a node is larger than all elements in it's two subtrees.
-This guarantees that the largest element in always in the *root* node.
-
-### Python Implementation
-
-```python
 class MinHeap:
     def __init__(self, arr=None):
         self.heap = []
@@ -51,5 +41,31 @@ class MinHeap:
 
     def get_min(self):
         return self.heap[0] if len(self.heap) > 0 else None
-```
 
+def heapSort(arr):
+    heap = MinHeap(arr)
+    return [heap.extract_min() for i in range(len(heap.heap))]
+
+class TestHeap (unittest.TestCase):
+    def testHeapify(self):
+        # There are many valid arrays heapify could return
+        self.assertEqual(MinHeap([9, 11, 18, 13, 15, 14, 7, 8, 12, 9, 4, 6, 3]).heap, [3, 4, 6, 8, 9, 9, 7, 13, 12, 11, 15, 18, 14])
+
+    def testGetMin(self):
+        self.assertEqual(MinHeap([9, 11, 18, 13, 15, 14, 7, 8, 12, 9, 4, 6, 3]).get_min(), 3)
+        self.assertEqual(MinHeap([9, 11, 2, 13, 15, 14, 7, 8, 12, 9, 4, 6, 3]).get_min(), 2)
+
+    def testInsert(self):
+        heap = MinHeap([9, 11, 2, 13, 15, 14, 7, 8, 12, 9, 4, 6, 3])
+        self.assertEqual(heap.get_min(), 2)
+        heap.insert(1)
+        self.assertEqual(heap.get_min(), 1)
+
+    def testHeapSort(self):
+        self.assertEqual(heapSort([5, 2, 3, 4, 1]), [1, 2, 3, 4, 5])
+        self.assertEqual(heapSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(heapSort([5]), [5])
+        self.assertEqual(heapSort([]), [])
+
+if __name__ == '__main__':
+    unittest.main()
