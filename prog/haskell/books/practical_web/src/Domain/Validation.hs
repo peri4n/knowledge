4212c4ca9ed2c1f3bc0567846667e2841922b219
Fabian Bull <fabian@fbull.de>
2022-02-17 20:32:30 +0100

    Add tests

--- prog/haskell/books/practical_web/src/Domain/Validation.hs
+++ prog/haskell/books/practical_web/src/Domain/Validation.hs
@@ -1,5 +1,5 @@
 module Domain.Validation where
-  
+
 import ClassyPrelude
 import Text.Regex.PCRE.Heavy
 
@@ -10,3 +10,15 @@ validate constructor validations val =
   case concatMap (\f -> maybeToList $ f val) validations of
     [] -> Right $ constructor val
     errs -> Left errs
+
+rangeBetween :: (Ord a) => a -> a -> e -> Validation e a
+rangeBetween minRange maxRange msg val =
+  if val >= minRange && val <= maxRange then Nothing else Just msg
+
+lengthBetween :: (MonoFoldable a) => Int -> Int -> e -> Validation e a
+lengthBetween minLen maxLen msg val =
+  rangeBetween minLen maxLen msg (length val)
+
+regexMatches :: Regex -> e -> Validation e Text
+regexMatches regex msg val =
+  if val =~ regex then Nothing else Just msg
--- prog/haskell/books/practical_web/test/Spec.hs
+++ prog/haskell/books/practical_web/test/Spec.hs
@@ -1,2 +1,20 @@
+import ClassyPrelude
+import Test.Hspec
+import Domain.Validation
+import Text.Regex.PCRE.Heavy
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Validations" $ do
+    it "lengthBetween" $ do
+      lengthBetween 1 5 "err" ("12345"::Text) `shouldBe` Nothing
+      lengthBetween 1 5 "err" ("123456"::Text) `shouldBe` Just "err"
+    it "regexMatches" $ do
+      regexMatches [re|^hello|] "err" "hello world" `shouldBe` Nothing
+      regexMatches [re|^hello|] "err" "failed world" `shouldBe` Just "err"
+    it "validate" $ do
+      let mustContainA = regexMatches [re|A|] "Must contain 'A'"
+      let mustContainB = regexMatches [re|B|] "Must contain 'B'"
+      validate id [mustContainA, mustContainB] "abc" `shouldBe` Left ["Must contain 'A'", "Must contain 'B'"]
+      validate id [mustContainA, mustContainB] "ABc" `shouldBe` Right "ABc"
+
