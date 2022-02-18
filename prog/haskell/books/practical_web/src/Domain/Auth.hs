b959c028aaca88545a4fa6976a4072f335e9fa95
Fabian Bull <fabian@fbull.de>
2022-02-18 20:27:01 +0100

    Continue chapter 3 in practical_web

--- prog/haskell/books/practical_web/src/Domain/Auth.hs
+++ prog/haskell/books/practical_web/src/Domain/Auth.hs
@@ -1,6 +1,34 @@
-module Domain.Auth where
+module Domain.Auth (
+  -- * Types
+  Auth(..),
+  Email,
+  mkEmail,
+  rawEmail,
+  Password,
+  mkPassword,
+  rawPassword,
+  UserId,
+  VerificationCode,
+  SessionId,
+  RegistrationError(..),
+  EmailVerificationError(..),
+  LoginError(..),
+
+  -- * Ports
+  AuthRepo(..),
+  SessionRepo(..),
+  EmailVerificationNotif(..),
+
+  -- * Use cases
+  register,
+  verifyEmail,
+  login,
+  resolveSessionId,
+  getUser
+) where 
 
 import ClassyPrelude
+import Control.Monad.Except
 import Domain.Validation
 import Text.Regex.PCRE.Heavy
 
@@ -13,7 +41,7 @@ data Auth = Auth
 data RegistrationError = RegistrationErrorEmailTaken
   deriving (Show, Eq)
 
-newtype Email = Email {emailRaw :: Text} deriving (Show, Eq)
+newtype Email = Email {emailRaw :: Text} deriving (Ord, Show, Eq)
 
 rawEmail :: Email -> Text
 rawEmail = emailRaw
@@ -41,3 +69,49 @@ mkPassword =
       regexMatches [re|[A-Z]|] "Should contain uppercase letter",
       regexMatches [re|[a-z]|] "Should contain lowercase letter"
     ]
+
+type VerificationCode = Text
+
+data EmailVerificationError = EmailVerificationErrorInvalidCode deriving (Show, Eq)
+
+class Monad m => AuthRepo m where
+  addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+  findUserByAuth :: Auth -> m (Maybe (UserId, Bool))
+  setEmailAsVerified :: VerificationCode -> m (Either EmailVerificationError ())
+  findEmailFromUserId :: UserId -> m (Maybe Email)
+
+type UserId = Int
+
+type SessionId = Text
+
+data LoginError = LoginErrorInvalidAuth | LoginErrorEmailNotVerified deriving (Show, Eq)
+
+class Monad m => SessionRepo m where
+  newSession :: UserId -> m SessionId
+  findUserBySessionId :: SessionId -> m (Maybe UserId)
+
+class Monad m => EmailVerificationNotif m where
+  notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+register :: (AuthRepo m, EmailVerificationNotif m) => Auth -> m (Either RegistrationError ())
+register auth = runExceptT $ do
+  vCode <- ExceptT $ addAuth auth
+  let email = authEmail auth
+  lift $ notifyEmailVerification email vCode
+
+verifyEmail :: (AuthRepo m) => VerificationCode -> m (Either EmailVerificationError ())
+verifyEmail = setEmailAsVerified
+
+login :: (AuthRepo m, SessionRepo m) => Auth -> m (Either LoginError SessionId)
+login auth = runExceptT $ do
+  result <- lift $ findUserByAuth auth
+  case result of 
+    Nothing -> throwError LoginErrorInvalidAuth
+    Just (_, False) -> throwError LoginErrorEmailNotVerified
+    Just (uId, _) -> lift $ newSession uId
+
+resolveSessionId :: (SessionRepo m) => SessionId -> m (Maybe UserId)
+resolveSessionId = findUserBySessionId
+
+getUser :: (AuthRepo m) => UserId -> m (Maybe Email)
+getUser = findEmailFromUserId
