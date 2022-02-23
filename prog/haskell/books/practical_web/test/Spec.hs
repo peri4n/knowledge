3fe789d7f7c53c9d72617b9ba8d3ea251006713d
Fabian Bull <fabian@fbull.de>
2022-02-23 20:44:11 +0100

    Fix compile error

--- prog/haskell/books/practical_web/test/Spec.hs
+++ prog/haskell/books/practical_web/test/Spec.hs
@@ -1,12 +1,10 @@
 import ClassyPrelude
 import Test.Hspec
 import Domain.ValidationSpec
-import Adapter.InMemory.AuthSpec
-import Domain.AuthSpec
 import Text.Regex.PCRE.Heavy
+import Domain.AuthSpec
 
 main :: IO ()
 main = do
   validationSpec
   authSpec
-  inMemAdapterSpec
